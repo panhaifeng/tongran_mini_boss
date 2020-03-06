d01d25a34cdf1a88241dcb843830d8321e49e5fa
panhaifeng <613554008@qq.com>
2020-03-06 16:04:25 +0800

    数据库密码修改

---
+++ Config/compInfo.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+	'compName'=> '筒染小程序老板驾驶舱服务',
+	'verName'=> 'EQINFO SOFT',
+    'ENcompName'=> 'Technologies.Inc',
+	'domain_name'=> '',//配置本机服务器域名地址，也可以设置内网ip
+    'menu'=>'Config/menu.php',//使用的菜单目录
+	'hasProduct'=>true,//是否有产品档案
+    'dbDSN' => array(
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'login'     => 'root',
+        'password'  => 'phf571358',
+        'database'  => 'tongran_mini_boss',
+    ),
+);
+?>
